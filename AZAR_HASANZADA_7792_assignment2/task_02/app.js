ab755d1dc10ac136864a338513771d9f75c26198
ahasanzada <114696587+ahasanzada@users.noreply.github.com>
2023-07-02 14:51:35 +0400

    Task 2 otho() done without deleting scale

---
+++ AZAR_HASANZADA_7792_assignment2/task_02/app.js
@@ -0,0 +1,145 @@
+let gl, program;
+let vertexCount = 36;
+let modelViewMatrix;
+let projectionMatrix;
+
+let eye = [0, 0, 0.1];
+let at = [0, 0, 0];
+let up = [0, 1, 0];
+
+document.onkeydown = handleKeyDown;
+
+function handleKeyDown(event) {
+    let key = String.fromCharCode(event.keyCode);
+    
+    switch (key) {
+        case 'T':
+            eye = [0, 0, 1]; // Top
+            break;
+        case 'L':
+            eye = [-1, 0, 0]; // Left
+            break;
+        case 'F':
+            eye = [0, 0, 0.1]; // Front
+            break;
+        case 'D':
+            rotateCamera(-1); // Rotate clockwise
+            break;
+        case 'A':
+            rotateCamera(1); // Rotate counter-clockwise
+            break;
+    }
+}
+
+function rotateCamera(theta) {
+    let cosTheta = Math.cos(theta);
+    let sinTheta = Math.sin(theta);
+    let newUpX = cosTheta * up[0] - sinTheta * up[1];
+    let newUpY = sinTheta * up[0] + cosTheta * up[1];
+    up[0] = newUpX;
+    up[1] = newUpY;
+}
+
+onload = () => {
+    let canvas = document.getElementById("webgl-canvas");
+    
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) {
+        alert('No webgl for you');
+        return;
+    }
+
+    program = initShaders(gl, 'vertex-shader', 'fragment-shader');
+    gl.useProgram(program);
+
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    gl.enable(gl.DEPTH_TEST);
+
+    gl.clearColor(0, 0, 0, 0.5);
+
+    let vertices = [
+        -1, -1, 1,
+        -1, 1, 1,
+        1, 1, 1,
+        1, -1, 1,
+        -1, -1, -1,
+        -1, 1, -1,
+        1, 1, -1,
+        1, -1, -1,
+    ];
+
+    let indices = [
+        0, 3, 1,
+        1, 3, 2,
+        4, 7, 5,
+        5, 7, 6,
+        3, 7, 2,
+        2, 7, 6,
+        4, 0, 5,
+        5, 0, 1,
+        1, 2, 5,
+        5, 2, 6,
+        0, 3, 4,
+        4, 3, 7,
+    ];
+
+    let colors = [
+        0, 0, 0,
+        0, 0, 1,
+        0, 1, 0,
+        0, 1, 1,
+        1, 0, 0,
+        1, 0, 1,
+        1, 1, 0,
+        1, 1, 1,
+    ];
+
+    vertices = scale(0.5, vertices)
+
+    let vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+    let vPosition = gl.getAttribLocation(program, 'vPosition');
+    gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+    let iBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indices), gl.STATIC_DRAW);
+
+    let cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+    let vColor = gl.getAttribLocation(program, 'vColor');
+    gl.vertexAttribPointer(vColor, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
+
+    modelViewMatrix = gl.getUniformLocation(program, 'modelViewMatrix');
+    projectionMatrix = gl.getUniformLocation(program, 'projectionMatrix');
+
+    render();
+};
+
+function render() {
+    // Clipping volume parameters (scaled down)
+    let left = -1;
+    let right = 1;
+    let bottom = -1;
+    let top = 1;
+    let near = -5;
+    let far = 5;
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    let mvm = lookAt(eye, at, up);
+    let pm = ortho(left, right, bottom, top, near, far);
+
+    gl.uniformMatrix4fv(modelViewMatrix, false, flatten(mvm));
+    gl.uniformMatrix4fv(projectionMatrix, false, flatten(pm));
+
+    gl.drawElements(gl.TRIANGLES, vertexCount, gl.UNSIGNED_BYTE, 0);
+
+    requestAnimationFrame(render);
+}
